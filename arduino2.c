7b0fea02cbb80b168a8bb6e92bf1e58d31301ee2
rahmahkn <rahmah.ks.nurdini@gmail.com>
2022-04-24 09:49:07 +0700

    Succeed add 1 person

--- arduino2.c
+++ arduino2.c
@@ -1,20 +1,64 @@
 char unlocked[10]; //Initialized variable to store recieved data
+char unlockedTemp[10];
 int bulb = 3;
 int button = 2;
+int sensor = 4;
+int startOpen = 0;
+int OPEN_TIME = 10;
+int num_person = 0;
+bool door_open = false;
+bool bulb_high = false;
 
 void setup() {
   // Begin the Serial at 9600 Baud
   Serial.begin(9600);
   pinMode(bulb, OUTPUT);
   pinMode(button, INPUT);
+  pinMode(sensor, INPUT);
 }
 
 void loop() {
-  Serial.readBytes(unlocked, 8); //Read the serial data and store in var
-  Serial.println(unlocked); //Print data on Serial Monitor
-  
-  if (strcmp(unlocked, "Unlocked")) {
-    digitalWrite(bulb, HIGH);
+  if (Serial.available() && (num_person == 0)) {
+    Serial.readBytes(unlocked, 8); //Read the serial data and store in var
+    startOpen = millis();
+    door_open = true;
+    bulb_high = true;
   }
-  delay(1000);
+  
+  if (door_open && bulb_high) {
+    digitalWrite(bulb, HIGH);
+    delay(1000);
+    digitalWrite(bulb, LOW);
+    bulb_high = false;
+  }
+  
+  Serial.println(num_person);
+    
+  if (door_open) {
+    int valSensor = digitalRead(sensor);
+    Serial.println(valSensor);
+
+    if (valSensor == HIGH) {
+      num_person ++;
+      door_open = false;
+    } else {
+      int durationOpen = (millis() - startOpen) / 1000;
+
+      if (durationOpen > OPEN_TIME) {
+      door_open = false;
+      }
+    }
+  }
+  
+  // int valSensor = digitalRead(sensor);
+  // if (valSensor == HIGH) {
+  //   digitalWrite(bulb, HIGH);
+  //   delay(1000);
+  //   digitalWrite(bulb, LOW);
+  //   delay(1000);
+  // } else {
+  //   digitalWrite(bulb, LOW);
+  // }
+  
+  delay(100);
 }
